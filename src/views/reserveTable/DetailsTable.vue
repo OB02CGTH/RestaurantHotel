9ffd9fcbe6ae61069c5ff6fbebd976d1b9c8075c
soccer <121929081+tooneboy@users.noreply.github.com>
2023-01-27 11:08:30 +0700

    Add files via upload

---
+++ src/views/reserveTable/DetailsTable.vue
@@ -0,0 +1,196 @@
+<template>
+    <ion-page>
+      <ion-header :translucent="true">
+        <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-menu-button color="primary"></ion-menu-button>
+          </ion-buttons>
+          <ion-title>รายละเอียดผู้จอง</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      
+      <ion-content :fullscreen="true">
+        <ion-header collapse="condense">
+        
+        </ion-header>
+  
+       
+            
+      <ion-title2 size="large">รายละเอียดผู้จอง</ion-title2>      
+  
+     
+
+    
+  
+  <ion-item>
+    <ion-label position="floating">วันที่-เวลา</ion-label>
+    <ion-input placeholder="Enter text"></ion-input>
+  </ion-item>
+  <ion-item fill="solid">
+    <ion-label position="floating">ถึง-เวลา</ion-label>
+    <ion-input placeholder="Enter text"></ion-input>
+  </ion-item>
+  <ion-item>
+    <ion-label position="floating">ชื่อ-สกุล</ion-label>
+    <ion-input placeholder="กรุณากรอกชื่อ-สกุล"></ion-input>
+  </ion-item>
+  <ion-item>
+      <ion-label position="floating">จํานวน</ion-label>
+      <ion-input type="number" placeholder="0"></ion-input>
+    </ion-item>
+
+
+    <ion-item>
+      <ion-label position="floating">Email</ion-label>
+      <ion-input type="email" placeholder="email@domain.com"></ion-input>
+    </ion-item>
+
+    <ion-item>
+      <ion-label position="floating">เบอร์ติดต่อ</ion-label>
+      <ion-input type="tel" placeholder="888-888-8888"></ion-input>
+    </ion-item>
+ 
+  </ion-content>
+  
+        
+              <ion-button fill="clear" color="danger" routerLink="/folder/reserveTable">
+              <ion-icon ></ion-icon>
+          ย้อนกลับ
+            </ion-button> 
+      
+      <ion-button expand="block" color="success" routerLink="/folder/summarytable">
+              <ion-icon slot="start" :icon="addCircle"></ion-icon>
+             จอง
+            </ion-button> 
+    </ion-page>
+  </template>
+  <script lang="ts">
+  import {  defineComponent } from 'vue';
+  import { RouteLocationRaw } from 'vue-router';
+  import { IonNote,IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonItem, IonCard, IonCardContent, IonCardHeader, IonLabel, IonCheckbox, IonList, IonRadio, IonRadioGroup, IonListHeader, IonText, IonInput, IonIcon, } from '@ionic/vue';
+  import { star, addCircle,  removeCircle, } from 'ionicons/icons';
+  
+  export default defineComponent({
+    components: {
+      IonButton,
+      IonButtons,
+      IonContent,
+      IonHeader,
+      IonMenuButton,
+      IonPage,
+      IonTitle,
+      IonToolbar,
+      IonLabel,
+      IonItem,
+      IonInput,
+      // IonNote,
+      //  IonList,
+    },
+    data(){
+      return {
+       
+      }
+    },
+    
+    setup() {
+      return {
+        star,
+        addCircle,
+        removeCircle, 
+        value1: ''
+      }}
+    ,
+    methods:{
+      toroute(rou: RouteLocationRaw) {
+        this.$router.push(rou)
+      },
+      // validateEmail(email) {
+      //   return email.match(/^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/);
+      // }, 
+      //  validate(ev) {
+      //    const value = ev.target.value;
+
+      // this.$refs.item.$el.classList.remove('ion-valid');
+      //   this.$refs.item.$el.classList.remove('ion-invalid');
+
+      //   if (value === '') return;
+
+      //   this.validateEmail(value)
+      //   ? this.$refs.item.$el.classList.add('ion-valid')
+      //   : this.$refs.item.$el.classList.add('ion-invalid');
+      //  },
+
+      //  markTouched() {
+      //    this.$refs.item.$el.classList.add('ion-touched')}},
+
+ 
+    checkValue() {
+      if (this.value1 !== '11') {
+        this.value1 = ''
+      }
+    }
+  
+}
+
+       },
+  );
+  </script>
+  
+  <style scoped>
+  #container {
+    text-align: center;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  
+  
+  
+  #container strong {
+    font-size: 20px;
+    line-height: 26px;
+  }
+  
+  #container p {
+    font-size: 16px;
+    line-height: 22px;
+    color: #8c8c8c;
+    margin: 0;
+  }
+  
+  #container a {
+    text-decoration: none;
+  }
+  ion-title2{
+    padding: 45px;
+    font-size: 30px;
+    line-height:80px;
+    
+  }
+  ion-datetime-button
+  {
+    line-height:80px;
+    
+    
+  }
+  ion-card-header {
+    display: flex;
+    flex-flow: column-reverse;
+  }
+  
+  ion-item-group {
+    padding: 10px;
+  }
+  ion-datetime-button{
+    text-decoration: none;
+  }
+  
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+  }
+  </style>
+  
---
+++ src/views/reserveTable/searchTable.vue
@@ -0,0 +1,297 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button color="primary"></ion-menu-button>
+        </ion-buttons>
+        <ion-title>รายการการจองโต๊ะ</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">รายการการจองโต๊ะ</ion-title>
+        </ion-toolbar>
+      </ion-header>
+  <ion-segment :scrollable="true" :value="categorymenu[0].name">
+        <ion-segment-button v-for="i in categorymenu" :key="i.name" :value="i.name" @click="filterOrder(i.statorder)">
+          <ion-label>{{ i.name }}</ion-label>
+        </ion-segment-button>
+      </ion-segment>
+     
+<!-- <ion-searchbar  v-if="i.statorder === 1" placeholder="ค้นหาโต็ะ" ></ion-searchbar> -->
+
+      <div>
+     <img src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/128/calendar-icon.png" class="image-preview2"> 
+     <button v-bind:lang="language">{{ buttonText }}</button>
+     <ion-datetime-button datetime="datetime"></ion-datetime-button>
+
+
+ 
+
+
+       <ion-button color="success" routerLink="/folder/detailsTable" >จองเพิ่ม</ion-button>   
+  <ion-modal :keep-contents-mounted="true">
+    <ion-datetime id="datetime"></ion-datetime>
+  </ion-modal>
+  </div> 
+  <!-- <ion-button fill="outline">ลบ</ion-button>     -->
+    
+  
+
+
+
+
+
+
+  
+      <ion-grid>
+        <ion-row> 
+             <ion-col :sizeXs="12" :sizeMd="6" v-for="i,indexi in filteredOrder" :key="indexi">
+            <ion-card>
+
+              <ion-card-header v-if="i.statorder != 3">
+                  <ion-card-title>{{ i.ordertype }}: {{ i.ordernum }}</ion-card-title>
+              </ion-card-header>
+              <ion-card-header v-else color="danger">
+                  <ion-card-title>{{ i.ordertype }}: {{ i.ordernum }}</ion-card-title>
+              </ion-card-header>
+             
+              <ion-card-content>
+                <ion-item lines="none" v-for="(n, indexn) in i.menu" :key="indexn">
+                  <ion-label v-if="n.statmenu != 4" slot="start">{{ n.name }}</ion-label>
+                  <ion-label v-else slot="start" color="danger">{{ n.name }}</ion-label>
+                  <ion-label v-if="n.statmenu != 4" slot="end">{{  n.quantity }}</ion-label>
+                  <ion-label v-else slot="end" color="danger">{{  n.quantity }}</ion-label>
+
+                </ion-item>
+              
+              </ion-card-content>
+              <div class="container">
+    <ion-button  v-if="i.statorder === 1" fill="clear" color="danger" @click="presentActionSheet">เเก้ไข</ion-button>
+    <code>{{ result }}</code>
+  </div>
+  
+              <ion-button  v-if="i.statorder === 1" expand="block" color="success">ถึงร้านเเล้ว</ion-button>
+              <!-- <ion-button v-if="i.statorder === 3" expand="block" color="warning">แก้ไขออเดอร์</ion-button> -->
+              
+              <!-- <div v-if="i.statorder === 3">
+                <ion-grid>
+                  <ion-row>
+                    <ion-col :sizeXs="4">
+                      <ion-button expand="block" color="secondary" routerLink="/folder/MenuPage">สั่งเพิ่ม</ion-button>
+                    </ion-col>
+                    <ion-col :sizeXs="8">
+                      <ion-button expand="block" color="success">ชำระ</ion-button>
+                    </ion-col>
+                  </ion-row>
+                </ion-grid>
+              </div> -->
+
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+    </ion-content>
+<!-- 
+    <ion-footer v-if="filteredOrder[0].statorder === 3">
+      <ion-toolbar>
+        <ion-button v-show="isChecked = false" :disabled="true" expand="block" color="primary">ชำระหลายบิล</ion-button>
+        <ion-button v-show="isChecked = true" expand="block" color="primary">ชำระหลายบิล</ion-button>
+      </ion-toolbar>
+    </ion-footer> -->
+
+  </ion-page>
+</template>
+
+<script lang="ts">
+// import { Item } from '@ionic/core/dist/types/components/item/item';
+import { ref, defineComponent } from 'vue';
+import { RouteLocationRaw, useRoute } from 'vue-router';
+import { actionSheetController,
+  IonDatetime, IonDatetimeButton, IonModal, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonCol, IonGrid, IonRow, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonLabel, IonSegment, IonSegmentButton, IonItem, IonButton, IonCheckbox, IonFooter, IonText, IonToggle,
+} from '@ionic/vue';
+import { Item } from '@ionic/core/dist/types/components/item/item';
+import { pricetag } from 'ionicons/icons';
+
+export default defineComponent({
+  components: {
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonMenuButton,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonCol,
+    IonGrid,
+    IonRow,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    // IonCardSubtitle,
+    IonCardContent,
+    // IonSearchbar,
+    IonLabel,
+    // IonSegment,
+    // IonSegmentButton,
+    IonItem,
+    IonButton,
+    // IonCheckbox,
+    // IonFooter,
+    // IonText,
+    // IonToggle,
+    IonDatetime, IonDatetimeButton, IonModal,
+    
+  },
+  
+  setup() {
+      const result = ref('');
+
+      const presentActionSheet = async () => {
+        const actionSheet = await actionSheetController.create({
+        
+          buttons: [
+            {
+              text: 'เเก้ไข',
+              data: {
+                action: 'correct',
+              },
+            },
+            {
+              text: 'ลบ',
+              role: 'destructive',
+              data: {
+                action: 'delete',
+              },
+            },
+          ],
+        });
+
+        await actionSheet.present();
+
+        const res = await actionSheet.onDidDismiss();
+        result.value = JSON.stringify(res, null, 2);
+      };
+
+      return {
+        result,
+        presentActionSheet,
+      };
+    },
+  
+
+  data() {
+    return {
+      
+      imageSrc: 'image.jpg',
+      buttonText: 'Submit',
+    
+      ordermenu: [
+        {
+          ordertype: 'รหัสการจอง',
+          ordernum: 'A01', 
+          menu: [
+            { name: 'เวลา', quantity: "25/1/2566",  statmenu: 1, },
+            { name: 'ถึง', quantity: "25/1/2566", statmenu: 1, },
+            { name: 'ชื่อ-สกุล', quantity: "xxxx",  statmenu: 1, },
+            { name: 'เบอร์ติดต่อ', quantity: "xxxx", statmenu: 1, },
+            { name: 'จํานวน', quantity: 10, statmenu: 1, },
+          ],
+          statorder: 1,
+          // url: '/folder/Menu1',
+        },
+    
+        {
+          ordertype: 'รหัสการจอง',
+          ordernum: 'A05', 
+          menu: [
+            { name: 'เวลา', quantity: "15/1/2566",  statmenu: 1, },
+            { name: 'ถึง', quantity: "15/1/2566", statmenu: 1, },
+            { name: 'ชื่อ-สกุล', quantity: "xxxx",  statmenu: 1, },
+            { name: 'เบอร์ติดต่อ', quantity: "xxxx", statmenu: 1, },
+            { name: 'จํานวน', quantity: 15, statmenu: 1, },
+          ],
+          statorder: 2,
+          // url: '/folder/Menu1',
+        },
+      
+      ],
+      categorymenu: [
+        { name: 'กำลังเตรียม', statorder: 1, },
+        { name: 'ประวัติการจอง', statorder: 2, },
+       
+      ],
+      filteredOrder: {},
+    
+    }
+  },
+  methods: 
+    {toroute(rou: RouteLocationRaw) {
+      this.$router.push(rou)
+    },
+    filterOrder(iddata: number) {
+      console.log(iddata)
+      this.filteredOrder = this.ordermenu.filter(item => item.statorder === iddata)
+    },
+    // sumprice(menu: { name: string;  quantity: number; }[]) {
+    //   let sum = 0;
+    //   for (const i in menu) {
+    //     const menuobject = menu[i];
+    //     sum += menuobject.price * menuobject.quantity
+    //   }
+    //   return sum;
+    // },
+    
+  },
+  beforeMount() {
+    this.filterOrder(1)
+  },
+
+
+
+  
+ });
+</script>
+
+<style scoped>
+ .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    flex-direction: column;
+  }
+
+  code {
+    white-space: pre-wrap;
+  }
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+ion-col {
+  /* background-color: #3c4c4a; */
+  /* border: solid 0.5px #fff; */
+  border-radius: 5px;
+  color: #fff;
+  text-align: center;
+}
+
+.image-preview2 {
+  width: 29px;
+  height: 29px;
+}
+ion-card-header.ios {
+  display: flex;
+  flex-flow: column-reverse;
+}
+</style>
